eac0350f1adf1256fb249799bb3b1bfb9ff08033
Pham, Trung <72325861+pqtrng@users.noreply.github.com>
2021-08-15 18:08:27 +0700

    Init project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="src",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Anomaly detection on a credit card ",
+    author="Pham, Trung",
+)
